a441be8fae1d7d5a163484c269143a2e010dcbcb
Soo-Im <limsoo31@yonsei.ac.kr>
2021-11-18 23:23:14 +0900

    2021-11-18

--- start.js
+++ start.js
@@ -5,28 +5,42 @@ function addBlock(e) {
     e.preventDefault();
 
     const block = document.createElement("div");
-    block.class = "time-block";
+    block.classList.add("block");
+    const trackForm = document.createElement("form");
 
     const startTime = document.createElement("input");
     const endTime = document.createElement("input");
 
     startTime.type = "time";
     endTime.type = "time";
-    // startTime.required = True;
-    // endTime.required = True;
-    startTime.value = "08:30";  // TODO
+    setStartTime(startTime);
 
     const trackText = document.createElement("input");
-    trackText.type = "text";
-    
-    const trackForm = document.createElement("form");
+    trackText.type = "text";    
+
+    const submitBlock = document.createElement("button");
+    submitBlock.innerText = "입력";
+    submitBlock.addEventListener("click", addBlock);
 
     trackForm.appendChild(startTime);
     trackForm.appendChild(endTime);
     trackForm.appendChild(trackText);
+    trackForm.appendChild(submitBlock);
     
     block.appendChild(trackForm);
     trackDiv.appendChild(block);
 }
+
+function setStartTime(startTime) {
+    const allBlocks = document.querySelectorAll(".block");
+    if (allBlocks.length === 0) {
+        
+        startTime.value = startBtn.firstElementChild.value;
+    }
+    else {
+        startTime.value = "09:30";
+    }
+}
+
 
 startBtn.addEventListener("submit", addBlock);
