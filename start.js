efc6a2c042edc8eac7c019f384800a34635839ba
Soo-Im <limsoo31@yonsei.ac.kr>
2021-11-20 00:12:37 +0900

    2021-11-19

--- start.js
+++ start.js
@@ -9,11 +9,17 @@ function addBlock(e) {
     const trackForm = document.createElement("form");
 
     const startTime = document.createElement("input");
-    const endTime = document.createElement("input");
-
     startTime.type = "time";
-    endTime.type = "time";
-    setStartTime(startTime);
+    startTime.required = true;
+    startTime.step = "900";
+
+    const endTime = startTime.cloneNode();
+
+    startTime.classList.add("startTime");
+    endTime.classList.add("endTime");
+
+    initStartTime(startTime);
+    initEndTime(startTime, endTime);
 
     const trackText = document.createElement("input");
     trackText.type = "text";    
@@ -31,14 +37,33 @@ function addBlock(e) {
     trackDiv.appendChild(block);
 }
 
-function setStartTime(startTime) {
+function initEndTime(startTime, endTime) {
+    // code snippet from
+    // https://stackoverflow.com/questions/57179330/add-30min-to-entered-time
+    let hours = parseInt(startTime.value.split(':')[0]);
+    let minutes = parseInt(startTime.value.split(':')[1]);
+
+    minutes += 30;
+    if (minutes >= 60) {
+        hours = (hours + 1) % 24;
+        minutes -= 60;
+    }
+
+    // reformat values as strings with a fix length of 2
+    hours = (hours < 10 ? `0${hours}` : `${hours}`);
+    minutes = (minutes < 10 ? `0${minutes}` : `${minutes}`);
+    
+    endTime.value = `${hours}:${minutes}`;
+}
+
+function initStartTime(startTime) {
     const allBlocks = document.querySelectorAll(".block");
     if (allBlocks.length === 0) {
-        
         startTime.value = startBtn.firstElementChild.value;
     }
     else {
-        startTime.value = "09:30";
+        const latestTime = allBlocks[allBlocks.length-1].querySelector(".endTime").value;
+        startTime.value = latestTime;
     }
 }
 
