81ec6b02c70325cdf27110cde794d51f78858ff9
Soo-Im <limsoo31@yonsei.ac.kr>
2021-11-27 16:45:58 +0900

    2021-11-27

--- start.js
+++ start.js
@@ -112,16 +112,21 @@ function getIntervals() {
         endTimes.push(endTimeBlocks[i].value);
     }
 
-    startTimes = startTimes.split(":");
-    endTimes = endTimes.split(":");
-    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
-    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
-    var diff = endDate.getTime() - startDate.getTime();
-    var hours = Math.floor(diff / 1000 / 60 / 60);
-    diff -= hours * 1000 * 60 * 60;
-    var minutes = Math.floor(diff / 1000 / 60);
+    startTimes.map(function(start, i)
+    {
+        
+    });
 
-    return (hours < 9 ? "0" : "") + hours + ":" + (minutes < 9 ? "0" : "") + minutes;
+    // startTimes = startTimes.split(":");
+    // endTimes = endTimes.split(":");
+    // var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+    // var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+    // var diff = endDate.getTime() - startDate.getTime();
+    // var hours = Math.floor(diff / 1000 / 60 / 60);
+    // diff -= hours * 1000 * 60 * 60;
+    // var minutes = Math.floor(diff / 1000 / 60);
+
+    // return (hours < 9 ? "0" : "") + hours + ":" + (minutes < 9 ? "0" : "") + minutes;
 
     // const diffTimes = endTimes - startTimes;
     // const hh = Math.floor(diffTimes / 1000 / 60 / 60);
@@ -129,6 +134,7 @@ function getIntervals() {
     // const mm = Math.floor(diffTimes / 1000 / 60);
 
 }
+
 
 function aggregateTrack() {
     aggTimes = collectTimes();
